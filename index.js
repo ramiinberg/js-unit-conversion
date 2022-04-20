507f511a40186d33f91b4a0227304da08ccc616d
ramiinberg <rami.inberg@gmail.com>
2022-04-20 11:15:49 +0300

    added inputs

--- index.js
+++ index.js
@@ -7,14 +7,52 @@ let constLitersEl = document.getElementById("const-liters-el")
 let litersToGallonsEl = document.getElementById("liters-to-gallons-el")
 let constGallonsEl = document.getElementById("const-gallons-el")
 let gallonsToLitersEl = document.getElementById("gallons-to-liters-el")
+let constKgEl = document.getElementById("const-kg-el")
+let kgToPoundsEl = document.getElementById("kg-to-pounds-el")
+let constPoundsEl = document.getElementById("const-pounds-el")
+let poundsToKgEl = document.getElementById("pounds-to-kg-el")
 
-let count = countEl.textContent
-constMeterEl.textContent = count
-constFeetEl.textContent = count
-constGallonsEl.textContent = count
-constLitersEl.textContent = count
+const METERFEET = 3.28084
+const GALLONSLITERS = 3.785541
+const KGPOUNDS = 2.20462
 
-meterToFeetEl.textContent = (count * (1/0.3048)).toFixed(3)
-feetToMeterEl.textContent = (count * (0.3048/1)).toFixed(3)
-gallonsToLitersEl.textContent = (count * 3.785).toFixed(3)
-litersToGallonsEl.textContent = (count / 3.785).toFixed(3)
+function convert () {
+  value = countEl.value
+
+  updateValues(value)
+  console.log('test')
+}
+
+function updateValues(value) {
+  function initializeConstants() {
+    constMeterEl.textContent = value
+    constFeetEl.textContent = value
+    constGallonsEl.textContent = value
+    constLitersEl.textContent = value
+    constKgEl.textContent = value
+    constPoundsEl.textContent = value
+  }
+
+  function meterFeet() {
+    meterToFeetEl.textContent = (value * METERFEET).toFixed(3)
+    feetToMeterEl.textContent = (value / METERFEET).toFixed(3)
+  }
+
+  function gallonsLiters() {
+    gallonsToLitersEl.textContent = (value * GALLONSLITERS).toFixed(3)
+    litersToGallonsEl.textContent = (value / GALLONSLITERS).toFixed(3)
+  }
+
+  function kgPounds() {
+    kgToPoundsEl.textContent = (value * KGPOUNDS).toFixed(3)
+    poundsToKgEl.textContent = (value / KGPOUNDS).toFixed(3)
+  }
+
+  initializeConstants()
+  meterFeet()
+  gallonsLiters()
+  kgPounds()
+}
+
+updateValues(20)
+countEl.addEventListener("input", convert)
